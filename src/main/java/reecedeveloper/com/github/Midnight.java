ee94e6dc63079004e4396236751b23ec83c86aa5
ReeceDeveloper <ReeceDev03@protonmail.com>
2023-12-12 19:10:24 -0500

    Add JSON configuration

--- src/main/java/reecedeveloper/com/github/Midnight.java
+++ src/main/java/reecedeveloper/com/github/Midnight.java
@@ -1,7 +1,59 @@
 package reecedeveloper.com.github;
 
-public class Midnight {
-    public static void main(String[] args) {
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.exceptions.InvalidTokenException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reecedeveloper.com.github.configuration.Configuration;
 
+import java.time.Duration;
+import java.util.Scanner;
+
+public class Midnight {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Midnight.class);
+
+    public static void main(String[] args) {
+        try {
+            JDA jdaObject = JDABuilder.createDefault(
+                    Configuration.getConfigInstance("Configuration.json").optString("botToken")
+            ).build();
+
+            // Add the EventListener.
+
+            Scanner terminalScanner = new Scanner(System.in);
+
+            while (terminalScanner.hasNextLine()) {
+                String terminalCommand = terminalScanner.nextLine();
+
+                if (terminalCommand.equalsIgnoreCase("shutdown")) {
+                    LOGGER.info("Midnight will now initiate shutdown procedures.");
+
+                    jdaObject.shutdown();
+
+                    if (!jdaObject.awaitShutdown(Duration.ofMinutes(5))) {
+                        LOGGER.warn("JDA took more than 5 minutes to shut down gracefully, forcing shutdown.");
+
+                        jdaObject.shutdownNow();
+
+                        if (!jdaObject.awaitShutdown(Duration.ofMinutes(5))) {
+                            LOGGER.error("JDA took more than 5 minutes to forcibly shut down. Killing process.");
+
+                            System.exit(-1); // TODO: Perhaps setup a list of hex codes that indicate shutdown status?
+                        }
+                    }
+
+                    System.exit(0); // TODO: See previous TODO.
+                }
+            }
+        } catch (InvalidTokenException invalidTokenException) {
+            LOGGER.error("JDA initialization failed, invalid token.");
+
+            System.exit(-1); // TODO: See previous TODO.
+        } catch (InterruptedException threadInterruptedException) {
+            LOGGER.error("JDA thread interrupted during shutdown!");
+
+            System.exit(-1); // TODO: See previous TODO (not technically a failure, but non-graceful).
+        }
     }
 }
---
+++ src/main/java/reecedeveloper/com/github/configuration/Configuration.java
@@ -0,0 +1,33 @@
+package reecedeveloper.com.github.configuration;
+
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class Configuration extends JSONObject {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Configuration.class);
+
+    private static Configuration configInstance = null;
+
+    public Configuration(File jsonConfig) throws IOException {
+        super(new String(Files.readAllBytes(jsonConfig.toPath())));
+
+        configInstance = this;
+    }
+
+    public static Configuration getConfigInstance(String filePath) {
+        if (configInstance == null) {
+            try {
+                configInstance = new Configuration(new File(filePath));
+            } catch (IOException configLoadException) {
+                LOGGER.error("Unable to load the configuration file.");
+            }
+        }
+
+        return configInstance;
+    }
+}
